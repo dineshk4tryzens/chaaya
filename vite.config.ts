bb5037df9ce532b747415f173fb4f3c0ac2bc4b6
Ram Narayanan <ram.narayanan@tryzens.com>
2025-02-16 00:26:28 +0530

    Added preset vite config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from '@vercel/remix/vite';
+
 
 declare module "@remix-run/node" {
   interface Future {
@@ -23,6 +25,7 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
